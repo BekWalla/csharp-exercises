93ebd4b7206b01aa689ddff0ce89fd6086e90498
Rebekah Wallace <rebekahwallace03@gmail.com>
2017-02-27 17:52:11 -0600

    Prep exercises for class 1

---
+++ AreaRectangle/AreaRectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double len;
+            double wid;
+            double answer;
+
+            Console.WriteLine("What is the length of the rectangle?");
+            string length = Console.ReadLine();
+            len = double.Parse(length);
+            Console.WriteLine("What is the width of the rectangle?");
+            string width = Console.ReadLine();
+            wid = double.Parse(width);
+            answer = len * wid;
+            Console.WriteLine(answer);
+            Console.ReadLine();
+  
+        }
+    }
+}
---
+++ MilesToGallon/MilesToGallon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilesToGallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Miles;
+            double Gallons;
+            double milesPerGallon;
+
+            Console.WriteLine("How many miles have you traveled?");
+            string Mil = Console.ReadLine();
+            Miles = double.Parse(Mil);
+            Console.WriteLine("How many gallons of gas have you used?");
+            string Gal = Console.ReadLine();
+            Gallons = double.Parse(Gal);
+            milesPerGallon = Miles / Gallons;
+            Console.WriteLine(milesPerGallon);
+            Console.ReadLine();
+        }
+    }
+}
